3ffac6887516434c54de55b44fb45dcccf576a19
Yeregorix <yeregorixcraft@gmail.com>
2019-02-24 18:12:00 +0100

    Change version

--- src/main/java/net/smoofyuniverse/chaos/Chaos.java
+++ src/main/java/net/smoofyuniverse/chaos/Chaos.java
@@ -35,7 +35,7 @@ public class Chaos extends Application {
 	private UserInterface ui;
 
 	public Chaos(Arguments args) {
-		super(args, "Chaos", "1.0.3");
+		super(args, "Chaos", "1.0.4");
 	}
 
 	@Override
