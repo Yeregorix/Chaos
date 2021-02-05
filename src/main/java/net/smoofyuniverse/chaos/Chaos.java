b14ad69eb33ecdb2a59c0327ef1251926defe960
Yeregorix <yeregorixcraft@gmail.com>
2021-02-05 21:45:28 +0100

    Begin 1.0.11 development

--- src/main/java/net/smoofyuniverse/chaos/Chaos.java
+++ src/main/java/net/smoofyuniverse/chaos/Chaos.java
@@ -41,7 +41,7 @@ public class Chaos extends Application {
 	private UserInterface ui;
 
 	public Chaos(Arguments args) {
-		super(args, "Chaos", "1.0.10");
+		super(args, "Chaos", "1.0.11");
 	}
 
 	@Override
